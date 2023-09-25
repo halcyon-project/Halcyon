345dbdbec16b6fb3dcfdd2a0fccacb95e4ad66f4
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-09-25 16:12:12 -0400

    comment

--- src/main/resources/META-INF/public-web-resources/multi-viewer/src/filterPopup.js
+++ src/main/resources/META-INF/public-web-resources/multi-viewer/src/filterPopup.js
@@ -133,6 +133,7 @@ function createThresh(div, layers, viewer, colorPicker, classId) {
   function createInputHandler(updateElement) {
     return function() {
       updateElement.value = this.value;
+      // Layers, viewer, and threshold
       setFilter(layers, viewer, {}, { val: parseInt(this.value), rgba: getThreshColor(colorPicker), classId: classId });
     };
   }
@@ -193,7 +194,8 @@ function createDropdown(uniqueId, divArr, allLayers, viewer) {
 
     // Initial values set
     if (STATE.renderType === 'byThreshold') {
-      setFilter(allLayers, viewer, {}, { val: 1, rgba: [126, 1, 0, 255] }); // 128
+      // Layers, viewer, and threshold
+      setFilter(allLayers, viewer, {}, { val: 1, rgba: [126, 1, 0, 255] });
     } else {
       setFilter(allLayers, viewer);
     }
